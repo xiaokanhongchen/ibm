5c6e6e67907c1301cd8d085a7ffff2c451e1a01e
xiaokanhongchen <25289841+xiaokanhongchen@users.noreply.github.com>
2020-09-11 11:27:43 +0800

    Create v2ss.sh

---
+++ v2ss.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+read -p "请输入应用程序名称:" appname
+read -p "请设置你的容器内存大小(默认256):" ramsize
+if [ -z "$ramsize" ];then
+	ramsize=256
+fi
+rm -rf cloudfoundry
+mkdir cloudfoundry
+cd cloudfoundry
+
+echo '<!DOCTYPE html> '>>index.php
+echo '<html> '>>index.php
+echo '<body>'>>index.php
+echo '<?php '>>index.php
+echo 'echo "Hello World!"; '>>index.php
+echo '?> '>>index.php
+echo '<body>'>>index.php
+echo '</html>'>>index.php
+
+wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip -d v2 v2ray-linux-64.zip
+cd v2
+chmod 777 *
+cd ..
+rm -rf v2ray-linux-64.zip
+mv $HOME/cloudfoundry/v2ray1/v2ray $HOME/cloudfoundry/v2ss
+mv $HOME/cloudfoundry/v2ray1/v2ctl $HOME/cloudfoundry/v2ctl
+rm -rf $HOME/cloudfoundry/v2ray1
+echo '{"inbounds":[{"port":8080,"protocol":"shadowsocks","settings": {"method": "aes-128-gcm","ota": true,"password": "hongchenpasswd"}}],"outbounds": [{"protocol": "freedom","settings": {}}]}'>$HOME/cloudfoundry/config.json
+echo 'applications:'>>manifest.yml
+echo '- path: .'>>manifest.yml
+echo '  command: '/app/htdocs/v2ss'' >>manifest.yml
+echo '  name: '$appname''>>manifest.yml
+echo '  random-route: true'>>manifest.yml
+echo '  memory: '$ramsize'M'>>manifest.yml
+ibmcloud target --cf
+ibmcloud cf push
+domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
+cd ..
+echo 容器已经成功启动
+echo 地址: $domain
