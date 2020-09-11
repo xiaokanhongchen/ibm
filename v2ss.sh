d97524a9c53780d2d021bafe72f738102b85719e
xiaokanhongchen <25289841+xiaokanhongchen@users.noreply.github.com>
2020-09-11 16:26:17 +0800

    Update v2ss.sh

--- v2ss.sh
+++ v2ss.sh
@@ -26,7 +26,9 @@ rm -rf v2ray-linux-64.zip
 mv $HOME/cloudfoundry/v2/v2ray $HOME/cloudfoundry/v2ss
 mv $HOME/cloudfoundry/v2/v2ctl $HOME/cloudfoundry/v2ctl
 rm -rf $HOME/cloudfoundry/v2
-echo '{"inbounds":[{"port":8080,"protocol":"shadowsocks","settings": {"method": "aes-128-gcm","password": "hongchenpasswd"}}],"outbounds": [{"protocol": "freedom","settings": {}}]}'>$HOME/cloudfoundry/config.json
+uuid=`cat /proc/sys/kernel/random/uuid`
+path=`echo $uuid | cut -f1 -d'-'`
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/cloudfoundry/config.json
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
 echo '  command: '/app/htdocs/v2ss'' >>manifest.yml
@@ -36,6 +38,10 @@ echo '  memory: '$ramsize'M'>>manifest.yml
 ibmcloud target --cf
 ibmcloud cf push
 domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
+vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
 cd ..
 echo 容器已经成功启动
 echo 地址: $domain
+echo UUID: $uuid
+echo path: /$path
+echo vmess://$vmess
